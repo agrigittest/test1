c13d627f95988d2033664cbaffa0f1cde8e5aad8
agrigittest <service.informatique@agri-commerce.fr>
2017-07-31 18:49:17 +0200

    coucou

--- controllers/famille.js
+++ controllers/famille.js
@@ -25,8 +25,10 @@ function view_unefamille(id) {
     var self = this; // Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
     var User = MODEL('TFamille').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
-    User.findById(id, "-_id CodeFamille libFamille", function(err, user) { //find() methode de mongoDB pour rechercher 
-        //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
+    //User.findById(id, "-_id CodeFamille libFamille", function(err, user) { //find() methode de mongoDB pour rechercher 
+    //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
+    User.findOne({ CodeFamille: id }, "-_id CodeFamille libFamille", function(err, user) {
+
         self.json(user); //Methode de Total.js qui permet d'executer l'index.html  
     });
 
