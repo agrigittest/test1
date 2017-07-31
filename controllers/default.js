3e8f1ec7c7312b2ca541216ff259c4e3985692d9
agrigittest <service.informatique@agri-commerce.fr>
2017-07-31 18:27:09 +0200

    coucou

--- controllers/default.js
+++ controllers/default.js
@@ -1,16 +1,16 @@
 exports.install = function() {
-	F.route('/', view_index);
+    F.route('/famille', view_index);
 };
 
 // Read all users
 function view_index() {
 
-	var self = this;						// Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
-    var User = MODEL('TFamille').schema; 	// MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
+    var self = this; // Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
+    var User = MODEL('TFamille').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
-	User.find(function(err, users) {		//find() methode de mongoDB pour rechercher 
-		//self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
-		self.json(users);			//Methode de Total.js qui permet d'executer l'index.html  
-	});
+    User.find(function(err, users) { //find() methode de mongoDB pour rechercher 
+        //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
+        self.json(users); //Methode de Total.js qui permet d'executer l'index.html  
+    });
 
 }
