d10010904dded0a30e55b7db4bc6e906ba61f9fe
adm <adm@agri-commerce.fr>
2017-07-31 17:16:32 +0200

    test athan

--- models/TStock.js
+++ models/TStock.js
@@ -2,4 +2,4 @@
 var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({ CodeArticle:Number, CodeStock: Number,Qte: Number })
 exports.schema = mongoose.model('TStock', userSchema);
-exports.name = 'TStck';
+exports.name = 'TStock';
