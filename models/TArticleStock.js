faac2071eae91ab4b12057a82616fb6dcefbce01
Jeff Ciaravolo <jeff@agri-commerce.fr>
2017-07-31 16:57:08 +0200

    TArticleStock

---
+++ models/TArticleStock.js
@@ -0,0 +1,4 @@
+var mongoose = require('mongoose');
+var userSchema = mongoose.Schema({ CodeArticle:Number, CodeStock: Number,Qte: Number })
+exports.schema = mongoose.model('TArticleStock', userSchema);
+exports.name = 'TArticleStock';
