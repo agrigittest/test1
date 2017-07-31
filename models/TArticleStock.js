4c44e3ad471ff3ca432da2e0069b2ffde226a667
Jeff Ciaravolo <jeff@agri-commerce.fr>
2017-07-31 17:45:11 +0200

    Type Number , default 0

--- models/TArticleStock.js
+++ models/TArticleStock.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = mongoose.Schema({
     CodeArticle: Number,
     CodeStock: Number,
-    Qte: Number
+    Qte: {type: Number, default: 0}
 })
 exports.schema = mongoose.model('TArticleStock', userSchema);
 exports.name = 'TArticleStock';
