db41fd27f2679846be5e737c9c8f3f9cd7efeb24
Jeff Ciaravolo <jeff@agri-commerce.fr>
2017-07-31 18:22:22 +0200

    Commentaire

--- controllers/default.js
+++ controllers/default.js
@@ -5,11 +5,11 @@ exports.install = function() {
 // Read all users
 function view_index() {
 
-	var self = this;
-    var User = MODEL('TFamille').schema;
+	var self = this;						// Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
+    var User = MODEL('TFamille').schema; 	// MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
-	User.find(function(err, users) {
-		self.view('index', users);
+	User.find(function(err, users) {		//find() methode de mongoDB pour rechercher 
+		self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
 	});
 
 }
