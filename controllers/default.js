ad5ca6ff5d4fdc7910b4b7dc0fe63a82668b93e5
rossi66 <nicolas.figueres@agri-commerce.fr>
2017-07-31 18:33:47 +0200

    ohfie

--- controllers/default.js
+++ controllers/default.js
@@ -8,16 +8,9 @@ function view_index() {
     var self = this; // Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
     var User = MODEL('TFamille').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
-<<<<<<< HEAD
     User.find({}, "-_id CodeFamille libFamille", function(err, users) { //find() methode de mongoDB pour rechercher 
         //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
         self.json(users); //Methode de Total.js qui permet d'executer l'index.html  
     });
-=======
-	User.find(function(err, users) {		//find() methode de mongoDB pour rechercher 
-		//self.view('index', users);		//Methode de Total.js qui permet d'executer l'index.html  
-		self.json(users);					//Methode de Total.js qui permet de renvoyer du json...
-	});
->>>>>>> 253b9aa12ae6ac55836ea27b07f2d684e528729b
 
 }
