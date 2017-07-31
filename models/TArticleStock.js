cfdc0e5ac04cd758de25eea3a373c69d39b07dde
rossi66 <nicolas.figueres@agri-commerce.fr>
2017-07-31 17:49:10 +0200

    Modif

--- models/TArticleStock.js
+++ models/TArticleStock.js
@@ -1,9 +1,9 @@
 'use strict';
 var mongoose = require('mongoose');
-var userSchema = mongoose.Schema({
-    CodeArticle: Number,
-    CodeStock: Number,
-    Qte: Number
+var articleStockSchema = mongoose.Schema({
+    CodeArticle: { type: Number },
+    CodeStock: { type: Number },
+    Qte: { type: Number, default: 0 }
 })
-exports.schema = mongoose.model('TArticleStock', userSchema);
+exports.schema = mongoose.model('TArticleStock', articleStock);
 exports.name = 'TArticleStock';
