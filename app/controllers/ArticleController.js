6b91b40fbe200eced95a5e795b044cb3eca4f64e
agrigittest <service.informatique@agri-commerce.fr>
2017-08-01 16:37:09 +0200

    coucou

---
+++ app/controllers/ArticleController.js
@@ -0,0 +1,99 @@
+"use strict";
+
+MetronicApp.controller('ArticleController', ['$scope', '$rootScope', '$http', 'ArticlesRessource',
+    function($scope, $rootScope, $http, ArticlesRessource) {
+
+        $scope.article = {};
+
+        $scope.$on('$viewContentLoaded', function() {
+            // initisalie core components
+            Metronic.initAjax();
+
+            //Parametres propre au module Angular qui faut initialiser
+            //$rootScope.setting.layout.pageSidebarClosed = true;
+            //$rootScope.setting.layout.pageBodySolid = false;
+
+            //Appel à la méthode find afin d'afficher les familles
+            $scope.findOne();
+
+        });
+
+        //Méthode find qui utilise la ressource Famille,
+        // Cette ressource va renvoyer les familles de la base
+        $scope.findOne = function() {
+            FamillesRessource.get({
+                id: $rootScope.$stateParams.id
+            }, function(data) {
+                console.log(data); //Affichage de la data dans la console du serveur
+                $scope.article = data; // alimentation des données issues de la ressource Famille dans la variable familles
+
+            });
+        };
+
+        $scope.update = function() {
+            $scope.article.$update(function(data) {
+                //réinjection des valeurs enregistres par le serveur dans le formulaire du client
+                console.log(data);
+                $scope.findOne();
+            });
+        };
+        $scope.create = function() {
+            var article = new FamillesRessource($scope.article);
+            article.$save(function(data) {
+                console.log(data);
+                //Fonction qui transfère vers la page de modification du nouvel élément créé
+                $rootScope.$state.go("article.show", { id: data._id });
+            });
+        };
+
+
+    }
+]);
+
+MetronicApp.controller('ArticleListController', ['$scope', '$rootScope', '$http', 'ArticlesRessource',
+    // Crée le controleur avec en 
+    // premier paramètre le nom du controleur, 
+    // un tableau de paramètre possibles  
+    // $scope = C'est la donnee utilisable dans le formulaire (DOM limité )
+    // $rootScope =  C'est une variable globale (Exemple la session)
+    // $http = 
+    // Familles Ressource = (pas de $ car c'est les notres... (pas ceux d'angular comme scope))
+    // $timeout =  
+    // $filter = 
+
+    function($scope, $rootScope, $http, ArticlesRessource) {
+
+        $scope.articles = [];
+
+        $scope.$on('$viewContentLoaded', function() {
+            // initisalie core components
+            Metronic.initAjax();
+
+            //Parametres propre au module Angular qui faut initialiser
+            //$rootScope.setting.layout.pageSidebarClosed = true;
+            //$rootScope.setting.layout.pageBodySolid = false;
+
+            //Appel à la méthode find afin d'afficher les familles
+            $scope.find();
+
+        });
+
+        //Méthode find qui utilise la ressource Famille,
+        // Cette ressource va renvoyer les familles de la base
+        $scope.find = function() {
+            ArticlesRessource.query({}, function(data) {
+                console.log(data); //Affichage de la data dans la console du serveur
+                $scope.articles = data.data; // alimentation des données issues de la ressource Famille dans la variable familles
+
+            });
+        };
+        $scope.delete = function(article) {
+            ArticlesRessource.delete({ id: article._id }, function(data) {
+                console.log(data);
+                $scope.find();
+            });
+        };
+
+
+    }
+]);
---
+++ app/resources/articlesRessource.js
@@ -0,0 +1,34 @@
+"use strict";
+
+/*
+GET : Accède à une ressource
+HEAD : Accède aux métadonnées d’une ressource
+POST : Ajoute une ressource
+PUT : Met à jour une ressource complète en la remplaçant par une nouvelle version (99% des cas).
+PATCH : Met à jour une partie d’une ressource en envoyant un différentiel (une sorte de ‘git diff’)
+DELETE : Supprime une ressource
+*/
+
+/*
+Les méthode create, query, get sont déjà dans Angular, nous n'avons aps besoin de les réécrire.
+*/
+
+
+//Fichier ressource qui va contenir les requêtes 
+
+
+//On utilise toujours MetronicApp pour Angular
+//Factory est un service méthode qui va checker des URLS (que du REST)
+//
+MetronicApp.factory("ArticlesRessource", ['$resource',
+    function($resource) {
+        return $resource('/article/:id', {
+            id: '@_id'
+        }, {
+            query: { method: 'GET', isArray: false },
+            update: { method: 'PUT' },
+            delete: { method: 'DELETE' }
+        });
+    }
+
+]);
