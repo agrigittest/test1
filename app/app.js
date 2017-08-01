bdd9e871053957c5286e80ddf66f0b46b46e7733
agrigittest <service.informatique@agri-commerce.fr>
2017-08-01 18:01:11 +0200

    rodo

--- app/app.js
+++ app/app.js
@@ -361,7 +361,7 @@ MetronicApp.controller('sendEmailCtrl', ['$scope', '$modalInstance', 'data', fun
 
 
 /***
- Layout Partials.
+ Layout Partials.uy
  By default the partials are loaded through AngularJS ng-include directive. In case they loaded in server side(e.g: PHP include function) then below partial 
  initialization can be disabled and Layout.init() should be called on page load complete as explained above.
  ***/
