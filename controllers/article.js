9bc4b2fdd38e54c513b068b39fdc3d899979c4be
rossi66 <nicolas.figueres@agri-commerce.fr>
2017-08-01 07:43:40 +0200

    dzzdsz

--- controllers/article.js
+++ controllers/article.js
@@ -12,9 +12,9 @@ function view_index() {
     var self = this; // Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
     var articles = MODEL('TArticle').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
-    articles.find(function(err, users) { //find() methode de mongoDB pour rechercher 
+    articles.find(function(err, arts) { //find() methode de mongoDB pour rechercher 
         //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
-        self.json(articles); //Methode de Total.js qui permet d'executer l'index.html  
+        self.json(arts); //Methode de Total.js qui permet d'executer l'index.html  
     });
 
 }
