27c382fdb810fb90b6059f64b71655c4f1b9763d
rossi66 <nicolas.figueres@agri-commerce.fr>
2017-08-01 07:38:12 +0200

    modif controler

--- controllers/article.js
+++ controllers/article.js
@@ -10,7 +10,7 @@ exports.install = function() {
 function view_index() {
 
     var self = this; // Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
-    var articles = MODEL('TArticle').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
+    var articles = MODEL('Tarticle').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
     articles.find({}, "_id codeArticle libArticle prix codeFamille", function(err, users) { //find() methode de mongoDB pour rechercher 
         //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
