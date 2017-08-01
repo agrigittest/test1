90cf681e3c8ef8f3c8fde404e21e09c864a80a7d
agrigittest <service.informatique@agri-commerce.fr>
2017-08-01 08:00:18 +0200

    Stock

--- models/TStock.js
+++ models/TStock.js
@@ -1,5 +1,9 @@
 "use strict";
 var mongoose = require('mongoose');
-var userSchema = mongoose.Schema({ CodeArticle:Number, CodeStock: Number,Qte: Number })
+var userSchema = mongoose.Schema({
+    CodeArticle: Number,
+    CodeStock: Number,
+    Qte: Number
+})
 exports.schema = mongoose.model('TStock', userSchema);
 exports.name = 'TStock';
