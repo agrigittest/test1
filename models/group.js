6643261573c29433ae76f2b8cdcf0d72db8fa0b5
root <nicolas.figueres@agri-commerce.fr>
2017-08-01 14:19:31 +0200

    tm

---
+++ models/group.js
@@ -0,0 +1,31 @@
+"use strict";
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    timestamps = require('mongoose-timestamp');
+
+/**
+ * UserGroup Schema
+ */
+var userGroupSchema = new Schema({
+    name: { type: String, unique: true, trim: true },
+    _id: { type: String, lowercase: true, trim: true },
+    _createdAt: { type: Date },
+    updatedAt: Date,
+    notes: String,
+    rights: {
+        type: Schema.Types.Mixed
+    },
+    objectifs: {
+        type: Schema.Types.Mixed
+    },
+    isremoved: Boolean
+});
+
+userGroupSchema.plugin(timestamps);
+
+exports.Schema = mongoose.model('group', userGroupSchema, 'UserGroup');
+exports.name = 'group';
