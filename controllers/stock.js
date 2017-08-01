55480d161dca0680cd6e0e251b6e271407612fd9
agrigittest <service.informatique@agri-commerce.fr>
2017-08-01 08:02:19 +0200

    stock controler

--- controllers/stock.js
+++ controllers/stock.js
@@ -12,7 +12,7 @@ function view_index() {
     var self = this; // Permet de referencer le contexte si on change de niveau (Le fameux THAT .. principe de l'inception)
     var Stock = MODEL('TStock').schema; // MODEL est un mot clé reconnu par Total.js , le module doit se trouver dans le repertoire modules
 
-    Stock.find({}, "_id CodeStocl LibStock", function(err, stock) { //find() methode de mongoDB pour rechercher 
+    Stock.find({}, "_id CodeStock LibStock", function(err, stock) { //find() methode de mongoDB pour rechercher 
         //self.view('index', users);			//Methode de Total.js qui permet d'executer l'index.html  
         self.json(stock); //Methode de Total.js qui permet d'executer l'index.html  
     });
